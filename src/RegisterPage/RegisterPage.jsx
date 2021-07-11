5bc1875e040ad35fa4abfce693c51e9c574bd566
Wez Ireland <7905049+wezireland@users.noreply.github.com>
2021-07-10 17:31:08 -0700

    confirmed username changed to email

--- src/RegisterPage/RegisterPage.jsx
+++ src/RegisterPage/RegisterPage.jsx
@@ -48,44 +48,44 @@ class RegisterPage extends React.Component {
 		const { user, submitted } = this.state;
 		return (
 			<div className="col-md-6 col-md-offset-3">
-					<h2>Register</h2>
-					<form name="form" onSubmit={this.handleSubmit}>
-						<div className={'form-group' + (submitted && !user.given_name ? ' has-error' : '')}>
-							<label htmlFor="given_name">First Name</label>
-							<input type="text" className="form-control" name="given_name" value={user.given_name} onChange={this.handleChange} />
-							{submitted && !user.given_name &&
-								<div className="help-block">First Name is required</div>
-							}
-						</div>
-						<div className={'form-group' + (submitted && !user.family_name ? ' has-error' : '')}>
-							<label htmlFor="family_name">Last Name</label>
-							<input type="text" className="form-control" name="family_name" value={user.family_name} onChange={this.handleChange} />
-							{submitted && !user.family_name &&
-								<div className="help-block">Last Name is required</div>
-							}
-						</div>
-						<div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
-							<label htmlFor="email">Email</label>
-							<input type="text" className="form-control" name="email" value={user.email} onChange={this.handleChange} />
-							{submitted && !user.email &&
-								<div className="help-block">Email is required</div>
-							}
-						</div>
-						<div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-							<label htmlFor="password">Password</label>
-							<input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-							{submitted && !user.password &&
-									<div className="help-block">Password is required</div>
-							}
-						</div>
-						<div className="form-group">
-							<button className="btn btn-primary">Register</button>
-							{registering && 
-								<img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-							}
-							<Link to="/login" className="btn btn-link">Cancel</Link>
-						</div>
-					</form>
+				<h2>Register</h2>
+				<form name="form" onSubmit={this.handleSubmit}>
+					<div className={'form-group' + (submitted && !user.given_name ? ' has-error' : '')}>
+						<label htmlFor="given_name">First Name</label>
+						<input type="text" className="form-control" name="given_name" value={user.given_name} onChange={this.handleChange} />
+						{submitted && !user.given_name &&
+							<div className="help-block">First Name is required</div>
+						}
+					</div>
+					<div className={'form-group' + (submitted && !user.family_name ? ' has-error' : '')}>
+						<label htmlFor="family_name">Last Name</label>
+						<input type="text" className="form-control" name="family_name" value={user.family_name} onChange={this.handleChange} />
+						{submitted && !user.family_name &&
+							<div className="help-block">Last Name is required</div>
+						}
+					</div>
+					<div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+						<label htmlFor="email">Email</label>
+						<input type="text" className="form-control" name="email" value={user.email} onChange={this.handleChange} />
+						{submitted && !user.email &&
+							<div className="help-block">Email is required</div>
+						}
+					</div>
+					<div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+						<label htmlFor="password">Password</label>
+						<input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
+						{submitted && !user.password &&
+								<div className="help-block">Password is required</div>
+						}
+					</div>
+					<div className="form-group">
+						<button className="btn btn-primary">Register</button>
+						{registering && 
+							<img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+						}
+						<Link to="/login" className="btn btn-link">Cancel</Link>
+					</div>
+				</form>
 			</div>
 		);
 	}
